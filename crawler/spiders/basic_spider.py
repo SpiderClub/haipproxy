591feb22d51c7e6651004206ca80ba6cafc8aee0
resolvewang <1796246076@qq.com>
2018-01-21 20:40:11 +0800

    beautify the struct of mixin

--- crawler/spiders/basic_spider.py
+++ crawler/spiders/basic_spider.py
@@ -1,27 +1,17 @@
 """
-Common ip proxy spider.
-Here are common proxy website to crawl:
-https://www.kuaidaili.com/
-http://www.kxdaili.com/
-http://www.nianshao.me/
-http://www.goubanjia.com/
-http://www.xicidaili.com/
+Basic proxy ip crawler.
+
 """
 from config.settings import SPIDER_COMMON_TASK
 from ..redis_spiders import RedisSpider
 from ..items import ProxyUrlItem
-from .mixin import IPSourceMixin
+from .mixin import BaseSpider
 
 
 # notice multi inheritance order in python
-class CommonSpider(IPSourceMixin, RedisSpider):
+class CommonSpider(BaseSpider, RedisSpider):
     name = 'common'
     task_type = SPIDER_COMMON_TASK
-    # slow down each spider
-    custom_settings = {
-        'CONCURRENT_REQUESTS_PER_DOMAIN': 1,
-        'DOWNLOAD_DELAY': 3
-    }
 
     def parse(self, response):
         infos = response.xpath('//tr')[1:]
@@ -34,20 +24,6 @@ class CommonSpider(IPSourceMixin, RedisSpider):
 
             for protocol in protocols:
                 yield ProxyUrlItem(url=self.construct_proxy_url(protocol, ip, port))
-
-    def procotol_extractor(self, detail):
-        # TODO it might be socks4, fix this case
-        if 'socks' in detail:
-            protocols = ['socks5']
-        # TODO find a better way to recongnize http and https protocol
-        elif 'http,https' in detail:
-            protocols = ['http', 'https']
-        elif 'https' in detail:
-            protocols = ['https']
-        else:
-            protocols = ['http']
-
-        return protocols
 
 
 
--- crawler/spiders/mixin.py
+++ crawler/spiders/mixin.py
@@ -3,6 +3,29 @@ Useful mixin class for all the spiders.
 """
 
 
-class IPSourceMixin:
+class BaseSpider:
+    # slow down each spider
+    custom_settings = {
+        'CONCURRENT_REQUESTS_PER_DOMAIN': 1,
+        'DOWNLOAD_DELAY': 3
+    }
+
+    def procotol_extractor(self, detail):
+        # TODO it might be socks4, fix this case
+        if 'socks' in detail:
+            protocols = ['socks5']
+        # TODO find a better way to recongnize both http and https protocol
+        elif 'http,https' in detail:
+            protocols = ['http', 'https']
+        elif 'https' in detail:
+            protocols = ['https']
+        else:
+            protocols = ['http']
+
+        return protocols
+
     def construct_proxy_url(self, scheme, ip, port):
         return '{}://{}:{}'.format(scheme, ip, port)
+
+
+
