99ba2f185eb951647fddf11d45746e9aea0dbf4f
resolvewang <1796246076@qq.com>
2018-01-21 18:49:48 +0800

    add multi task type

--- scheduler/producer.py
+++ scheduler/producer.py
@@ -2,23 +2,26 @@
 This module schedules ip fetching tasks according to config.rules.
 """
 from config.rules import URLS
-from config.settings import SPIDER_TASK_QUEUE
 from utils.connetion import get_redis_con
+from config.settings import SPIDER_COMMON_TASK
 
 
 class TaskScheduler:
     @classmethod
     def store_task_into_redis(cls):
         con = get_redis_con()
-        seeds = list()
         for detail in URLS:
             if not detail.get('enable'):
                 continue
-            start = detail.get('start', 1)
-            end = detail.get('end', 10)
+            start = detail.get('start', None)
+            end = detail.get('end', None)
             url_format = detail.get('url_format')
-            seeds.extend(url_format.format(page) for page in range(start, end+1))
-        con.lpush(SPIDER_TASK_QUEUE, *seeds)
+            task_type = detail.get('task_type', SPIDER_COMMON_TASK)
+            if not start and not end:
+                con.lpush(task_type, url_format)
+            else:
+                seeds = [url_format.format(page) for page in range(start, end+1)]
+                con.lpush(task_type, *seeds)
 
 
 if __name__ == '__main__':
