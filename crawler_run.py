a58eb48be57b072849a890c09267d1837f252ba2
resolvewang <1796246076@qq.com>
2018-01-26 14:34:58 +0800

    haipproxy v0.1

--- crawler_run.py
+++ crawler_run.py
@@ -6,11 +6,12 @@ from scrapy.crawler import CrawlerRunner
 from scrapy.utils.log import configure_logging
 from scrapy.utils.project import get_project_settings
 
-from crawler.spiders import CommonSpider, AjaxSpider
+from crawler.spiders import (
+    CommonSpider, AjaxSpider, CrawlSpider, GFWSpider, AjaxGFWSpider)
 
 
 all_spiders = [
-    CommonSpider, AjaxSpider
+    CommonSpider, AjaxSpider, CrawlSpider, GFWSpider, AjaxGFWSpider
 ]
 
 
