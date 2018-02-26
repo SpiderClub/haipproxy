181026b8f893f39cd5306b6becbddad72e361fc9
resolvewang <1796246076@qq.com>
2018-02-26 22:25:42 +0800

    remove gfw task

--- run.sh
+++ run.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
-python crawler_booter.py --usage crawler &
-python crawler_booter.py --usage validator &
-python scheduler_booter.py --usage crawler &
-python scheduler_booter.py --usage validator &
+python crawler_booter.py --usage crawler common ajax &
+python crawler_booter.py --usage validator init https &
+python scheduler_booter.py --usage crawler common ajax &
+python scheduler_booter.py --usage validator https &
 python squid_update.py
