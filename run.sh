1a815c64546590bba27af207a03d57e4433c99d8
resolvewang <1796246076@qq.com>
2018-03-08 19:53:40 +0800

    fix issue#1

--- run.sh
+++ run.sh
@@ -1,7 +1,8 @@
-#!/usr/bin/env bash
+#!/bin/bash
 nohup python crawler_booter.py --usage crawler common > crawler.log 2>&1 &
 nohup python scheduler_booter.py --usage crawler common > crawler_scheduler.log 2>&1 &
 nohup python crawler_booter.py --usage validator init > init_validator.log 2>&1 &
 nohup python crawler_booter.py --usage validator https > https_validator.log 2>&1&
 nohup python scheduler_booter.py --usage validator https > validator_scheduler.log 2>&1 &
-python squid_update.py --usage https --internal 3
+nohup python squid_update.py --usage https --internal 3 > squid.log 2>&1 &
+squid -N -d1
