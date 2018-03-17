418cece8913d54769bd55490fee0c5027d118874
resolvewang <1796246076@qq.com>
2018-03-17 17:11:09 +0800

    fix bugs for robbin strategy; support concurrency security

--- utils/__init__.py
+++ utils/__init__.py
@@ -2,7 +2,6 @@
 This module provides useful utils for haipproxy
 """
 from .functools import decode_all
-from .daemon import Daemon
 from .redis_util import (
     get_redis_conn, acquire_lock,
     release_lock)
