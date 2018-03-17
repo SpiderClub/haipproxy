8bcff0be8e0fabd31a14445457c7da46b38b84c0
resolvewang <1796246076@qq.com>
2018-03-17 15:07:49 +0800

    add daemon function to refresh ips

---
+++ utils/daemon.py
@@ -0,0 +1,107 @@
+"""
+Reference: http://blog.csdn.net/taiyang1987912/article/details/44850999
+"""
+import sys
+import os
+import time
+import atexit
+from signal import SIGTERM
+
+
+PID_FILE = os.path.dirname(os.path.dirname(__file__)) + '/pool_refresh.pid'
+
+
+class Daemon:
+    def __init__(self, pidfile=PID_FILE, stdin='/dev/null',
+                 stdout='/dev/null', stderr='/dev/null'):
+        self.stdin = stdin
+        self.stdout = stdout
+        self.stderr = stderr
+        self.pidfile = pidfile
+
+    def _daemonize(self):
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write('fork #1 failed: %d (%s)\n' % (e.errno, e.strerror))
+            sys.exit(1)
+
+        os.chdir("/")
+        os.setsid()
+        os.umask(0)
+
+        try:
+            pid = os.fork()
+            if pid > 0:
+                sys.exit(0)
+        except OSError as e:
+            sys.stderr.write('fork #2 failed: %d (%s)\n' % (e.errno, e.strerror))
+            sys.exit(1)
+
+        sys.stdout.flush()
+        sys.stderr.flush()
+        si = open(self.stdin, 'r', 1)
+        so = open(self.stdout, 'a+', 1)
+        se = open(self.stderr, 'a+', 1)
+        os.dup2(si.fileno(), sys.stdin.fileno())
+        os.dup2(so.fileno(), sys.stdout.fileno())
+        os.dup2(se.fileno(), sys.stderr.fileno())
+
+        atexit.register(self.delpid)
+        pid = str(os.getpid())
+        open(self.pidfile, 'w+').write('%s\n' % pid)
+
+    def delpid(self):
+        os.remove(self.pidfile)
+
+    def start(self):
+        try:
+            pf = open(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if pid:
+            message = 'pidfile %s already exist. Daemon already running!\n'
+            sys.stderr.write(message % self.pidfile)
+            sys.exit(1)
+
+        self._daemonize()
+        self._run()
+
+    def stop(self):
+        try:
+            pf = open(self.pidfile, 'r')
+            pid = int(pf.read().strip())
+            pf.close()
+        except IOError:
+            pid = None
+
+        if not pid:
+            message = 'pidfile %s does not exist. Daemon not running!\n'
+            sys.stderr.write(message % self.pidfile)
+            return
+        try:
+            while 1:
+                os.kill(pid, SIGTERM)
+                time.sleep(0.1)
+        except OSError as err:
+            err = str(err)
+            if err.find('No such process') > 0:
+                if os.path.exists(self.pidfile):
+                    os.remove(self.pidfile)
+            else:
+                print(str(err))
+                print(sys.exit(1))
+
+    def restart(self):
+        self.stop()
+        self.start()
+
+    def _run(self):
+        """run your function"""
+        raise NotImplementedError
+
