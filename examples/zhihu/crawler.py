9cfcd8fc4824bfab560167de681851ddbd3e047a
resolvewang <1796246076@qq.com>
2018-03-20 10:37:43 +0800

    fix bugs when giving feedbacks to proxy fetcher

--- examples/zhihu/crawler.py
+++ examples/zhihu/crawler.py
@@ -15,41 +15,43 @@ class Crawler:
         'Host': 'www.zhihu.com'
     }
 
-    def __init__(self, proxy_mode=1, retries=5):
-        self.proxy_mode = proxy_mode
+    def __init__(self, retries=5):
         self.retries = retries
         self.fetcher = ProxyFetcher('zhihu', strategy='greedy')
         self.conn = get_redis_conn(db=1)
+        self.scheme = 'https'
 
     def get(self, url):
-        proxy = None
         tries = 0
         while tries < self.retries:
-            if self.proxy_mode:
-                proxy = {'https': self.fetcher.get_proxy()}
-                while not proxy:
-                    time.sleep(1)
-                    proxy = {'https': self.fetcher.get_proxy()}
+            proxy = {self.scheme: self.fetcher.get_proxy()}
+            while not proxy.get(self.scheme):
+                time.sleep(0.5)
+                proxy = {self.scheme: self.fetcher.get_proxy()}
 
             try:
                 start = time.time() * 1000
-                resp = requests.get(url, headers=self.headers, proxies=proxy, timeout=self.timeout)
+                resp = requests.get(url, headers=self.headers, proxies=proxy,
+                                    timeout=self.timeout, verify=False)
                 end = time.time() * 1000
                 if '安全验证' in resp.text:
-                    self.fetcher.proxy_feedback('failure', proxy)
+                    if proxy:
+                        self.fetcher.proxy_feedback('failure', proxy.get(self.scheme))
+                    print('Current ip is blocked! The proxy is {}'.format(proxy))
                     tries += 1
                     continue
                 else:
                     print('Request succeeded! The proxy is {}'.format(proxy))
                     # if you use greedy strategy, you must feedback
-                    self.fetcher.proxy_feedback('success', proxy, int(end-start))
+                    self.fetcher.proxy_feedback('success', proxy.get(self.scheme), int(end - start))
                     # not considering transaction
                     self.conn.incr(self.success_req, 1)
-                    self.conn.rpush(self.cur_time, int(end/1000))
+                    self.conn.rpush(self.cur_time, int(end / 1000))
                     return resp.text
             except Exception as e:
                 print(e)
+                print('Request failed!The proxy is {}'.format(proxy))
                 # it's important to feedback, otherwise you may use the bad proxy next time
-                self.fetcher.proxy_feedback('failure', proxy)
+                self.fetcher.proxy_feedback('failure', proxy.get(self.scheme))
             tries += 1
         return None
