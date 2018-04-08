23af84a69801ae17550942cbd450bacfd21346a6
zingdle <31171119+zingdle@users.noreply.github.com>
2018-04-08 20:28:03 +0800

    Update run.sh
    Fix #19

--- run.sh
+++ run.sh
@@ -5,4 +5,5 @@ nohup python crawler_booter.py --usage validator init > init_validator.log 2>&1 
 nohup python crawler_booter.py --usage validator https > https_validator.log 2>&1&
 nohup python scheduler_booter.py --usage validator https > validator_scheduler.log 2>&1 &
 nohup python squid_update.py --usage https --interval 3 > squid.log 2>&1 &
+rm -rf /var/run/squid.pid
 squid -N -d1
