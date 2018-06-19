7ebe02a0f729ca990a815043eced32347dee431b
resolvewang <1796246076@qq.com>
2018-06-19 19:42:51 +0800

    update squid init method

--- haipproxy/client/squid.py
+++ haipproxy/client/squid.py
@@ -7,7 +7,12 @@ import subprocess
 from ..utils import get_redis_conn
 from ..config.settings import (
     SQUID_BIN_PATH, SQUID_CONF_PATH,
-    SQUID_TEMPLATE_PATH)
+    SQUID_TEMPLATE_PATH, TTL_VALIDATED_RESOURCE,
+    LONGEST_RESPONSE_TIME, LOWEST_SCORE,
+    LOWEST_TOTAL_PROXIES)
+from ..config.rules import (
+    SCORE_MAPS, TTL_MAPS,
+    SPEED_MAPS)
 from .core import IPFetcherMixin
 
 
@@ -20,8 +25,18 @@ class SquidClient(IPFetcherMixin):
     other_confs = ['request_header_access Via deny all', 'request_header_access X-Forwarded-For deny all',
                    'request_header_access From deny all', 'never_direct allow all']
 
-    def __init__(self, task):
-        super().__init__(task)
+    def __init__(self, task, score_map=SCORE_MAPS,
+                 ttl_map=TTL_MAPS, speed_map=SPEED_MAPS,
+                 longest_response_time=LONGEST_RESPONSE_TIME, lowest_score=LOWEST_SCORE,
+                 ttl_validated_resource=TTL_VALIDATED_RESOURCE, min_pool_size=LOWEST_TOTAL_PROXIES):
+        if task not in score_map.keys():
+            # client_logger.warning('task value is invalid, https task will be used')
+            task = 'https'
+        score_queue = score_map.get(task)
+        ttl_queue = ttl_map.get(task)
+        speed_queue = speed_map.get(task)
+        super().__init__(score_queue, ttl_queue, speed_queue, longest_response_time,
+                         lowest_score, ttl_validated_resource, min_pool_size)
         self.template_path = SQUID_TEMPLATE_PATH
         self.conf_path = SQUID_CONF_PATH
         if not SQUID_BIN_PATH:
